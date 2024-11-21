2e1ced2e5043073e2f836683029c5e9874005de2
XendamIbrahimli <xandamei-bp215@code.edu.az>
2024-11-21 13:24:43 +0400

    First Task

---
+++ UniqloMVC/UniqloMVC/Program.cs
@@ -0,0 +1,20 @@
+namespace UniqloMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+                name:"default", 
+                pattern: "{controller=Home}/{action=Index}/{Id?}");
+
+            app.Run();
+        }
+    }
+}
